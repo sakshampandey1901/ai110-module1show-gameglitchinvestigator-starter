7e838676f9a42a8d12aa1fc7eef4813d5a713283
sakshampandey1901 <pandeysaksham450@gmail.com>
2026-03-04 22:41:41 -0500

    correcting the incorrect hint showcase

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ def check_guess(guess, secret):
 
     try:
         if guess > secret:
-            return "Too High", "📈 Go HIGHER!"
+            return "Too High", "📈 Go LOWER!"
         else:
-            return "Too Low", "📉 Go LOWER!"
+            return "Too Low", "📉 Go HIGHER!"
     except TypeError:
         g = str(guess)
         if g == secret:
